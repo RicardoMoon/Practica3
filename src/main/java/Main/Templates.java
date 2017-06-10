02ef582ca8a6e365dcc9ef7b0738ad108fdff2e5
Leny Luna <leny0023@hotmail.com>
2017-06-10 19:54:40 -0400

    Se creo la clase Tamples

---
+++ src/main/java/Main/Templates.java
@@ -0,0 +1,24 @@
+package Main;
+
+import freemarker.template.Configuration;
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import static spark.Spark.get;
+
+/**
+ * Created by Leny96 on 10/6/2017.
+ */
+public class Templates {
+    public void TempFreeMarker() {
+        Configuration configuration = new Configuration(Configuration.VERSION_2_3_23);
+        configuration.setClassForTemplateLoading(Templates.class, "/templates");
+        FreeMarkerEngine freeMarkerEngine = new FreeMarkerEngine(configuration);
+
+        get("/home/", (request, response) -> {
+
+            return new ModelAndView(null, "home.html");
+        }, freeMarkerEngine);
+    }
+
+}
