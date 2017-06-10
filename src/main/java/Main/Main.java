2beaec852c198fcc0e63d6116b8a14fd95453a6c
ricaluna <20131196@ce.pucmm.edu.do>
2017-06-10 18:07:07 -0400

    Clases logicas creadas

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,17 @@
+package Main;
+
+import static spark.Spark.staticFiles;
+import static spark.debug.DebugScreen.enableDebugScreen;
+
+/**
+ * Created by Adolfo on 10/6/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        staticFiles.location("/publico");
+
+        enableDebugScreen();
+
+
+    }
+}
